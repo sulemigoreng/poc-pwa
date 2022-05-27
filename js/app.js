883da379d83b32d1eeb740de564d71cc0f9230c4
regawarp <regawa04@gmail.com>
2022-05-27 16:38:35 +0700

    Implement Post Book Data when Offline

--- js/app.js
+++ js/app.js
@@ -91,25 +91,84 @@ function storeToDB(books) {
     };
 }
 
-async function postBooks({
-    title, synopsis, cover, releaseDate, 
+function postBookToDb({
+    title, synopsis, cover, releaseDate,
     author, genre, totalPage
 }) {
-    let url = 'https://glibrary-api.staging-gi.web.id/api/Books';
-    try {
-        let page = parseInt(totalPage, 10);
-        console.log(totalPage);
-        console.log(page);
-        let res = await fetch(url, {
-            method:'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({title, synopsis, cover, releaseDate, author, genre, totalPage:parseInt(totalPage, 10)})
+    console.log("postBookToDb");
+    // Open (or create) the database
+    var open = indexedDB.open("MyDatabase", 5);
+
+    // Create the schema
+    open.onupgradeneeded = function () {
+        var db = open.result;
+        var store = db.createObjectStore("BookObjectStore", { keyPath: "id" });
+    };
+
+    open.onsuccess = function () {
+        // Start a new transaction
+        var db = open.result;
+        var tx = db.transaction("BookObjectStore", "readwrite");
+        var store = tx.objectStore("BookObjectStore");
+
+        store.put({
+            "id": Date.now(),
+            "title": title,
+            "synopsis": synopsis,
+            "cover": cover,
+            "releaseDate": releaseDate,
+            "author": author,
+            "genre": genre,
+            "totalPage": totalPage,
+            "status": "Pending"
         });
-        return await res.json();
-    } catch (error) {
-        console.log(error);
+
+        // Close the db when the transaction is done
+        tx.oncomplete = function () {
+            db.close();
+        };
+    };
+
+    return {
+        "id": 99,
+        "title": title,
+        "synopsis": synopsis,
+        "cover": cover,
+        "releaseDate": releaseDate,
+        "author": author,
+        "genre": genre,
+        "totalPage": totalPage,
+        "status": "Pending"
+    };
+}
+
+async function postBooks({
+    title, synopsis, cover, releaseDate,
+    author, genre, totalPage
+}) {
+    if (navigator.onLine) {
+        let url = 'https://glibrary-api.staging-gi.web.id/api/Books';
+        try {
+            let page = parseInt(totalPage, 10);
+            console.log(totalPage);
+            console.log(page);
+            let res = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ title, synopsis, cover, releaseDate, author, genre, totalPage: parseInt(totalPage, 10) })
+            });
+            return await res.json();
+        } catch (error) {
+            console.log(error);
+        }
+    } else {
+        return await postBookToDb({
+            title, synopsis, cover, releaseDate,
+            author, genre, totalPage
+        });
     }
+
 }
