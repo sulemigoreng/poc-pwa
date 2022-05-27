937703026a08c74ffe48e1802795b5dd4f3b4069
sulemigoreng <sulemigoreng@gmail.com>
2022-05-27 09:55:41 +0700

    Make the web more responsive
    - Implement responsive display for mobile
    - Edit fallback html false absolute link (js,css, etc.)
    - Remove dummy html

--- js/ui.js
+++ js/ui.js
@@ -2,3 +2,18 @@ var btn_add = document.getElementById("btn-add");
 btn_add.addEventListener("click", () => {
     
 });
+
+// Mobile UI
+var btn_nav = document.getElementById("m-nav-btn");
+var btn_nav_back = document.getElementById("m-btn-back");
+var m_nav = document.getElementById("m-navbar");
+
+btn_nav.addEventListener("click", () => {
+    m_nav.classList.remove("m-navbar-hide");
+    m_nav.classList.add("m-navbar");
+});
+
+btn_nav_back.addEventListener("click", () => {
+    m_nav.classList.remove("m-navbar");
+    m_nav.classList.add("m-navbar-hide");
+});
